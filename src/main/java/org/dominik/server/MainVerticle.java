95f90a6a305e8f17253450f19d22ac98827e8453
Dominik Kreński <dominik.krenski@gmail.com>
2022-02-26 18:07:37 +0100

    MainVerticle refactored

--- src/main/java/org/dominik/server/MainVerticle.java
+++ src/main/java/org/dominik/server/MainVerticle.java
@@ -1,12 +1,17 @@
 package org.dominik.server;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Handler;
 import io.vertx.core.Promise;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonObject;
+import io.vertx.ext.auth.KeyStoreOptions;
+import io.vertx.ext.auth.jwt.JWTAuth;
+import io.vertx.ext.auth.jwt.JWTAuthOptions;
 import io.vertx.ext.mongo.MongoClient;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.BodyHandler;
 import org.dominik.server.errors.ApiError;
 import org.dominik.server.exceptions.BaseException;
@@ -14,7 +19,10 @@ import org.dominik.server.handlers.UserHandler;
 import org.dominik.server.services.definitions.ApiService;
 import org.dominik.server.services.implementations.ApiServiceImpl;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.time.Instant;
+import java.util.Properties;
 
 public class MainVerticle extends AbstractVerticle {
   private static final String LOGIN_URL = "/login";
@@ -29,7 +37,7 @@ public class MainVerticle extends AbstractVerticle {
     ApiService apiService = new ApiServiceImpl(createMongoClient());
 
     // create UserHandler instance
-    UserHandler userHandler = new UserHandler(apiService);
+    UserHandler userHandler = new UserHandler(apiService, createJwtProvider());
 
     // create router instance
     Router router = createRoutes(userHandler);
@@ -55,31 +63,72 @@ public class MainVerticle extends AbstractVerticle {
       .produces("application/json")
       .handler(BodyHandler.create())
       .handler(userHandler::register)
-      .failureHandler(e -> {
-        Throwable failure = e.failure();
+      .failureHandler(this::handleFailure);
 
-        ApiError apiError;
-
-        if (failure instanceof BaseException) {
-          var base = (BaseException) failure;
-          apiError = new ApiError(base.getStatusCode(), base.getTimestamp(), base.getMessage());
-        } else {
-          apiError = new ApiError(500, Instant.now(), "Something went wrong. Please, try later");
-        }
-
-        e.response().setStatusCode(apiError.getStatus()).end(Json.encode(apiError));
-      });
+    router
+      .post(LOGIN_URL)
+      .consumes("application/json")
+      .produces("application/json")
+      .handler(BodyHandler.create())
+      .handler(userHandler::login)
+      .failureHandler(this::handleFailure);
 
     return router;
   }
 
-  private MongoClient createMongoClient() {
+  private void handleFailure(RoutingContext ctx) {
+    Throwable failure = ctx.failure();
+    ApiError apiError;
 
+    if (failure instanceof BaseException) {
+      var base = (BaseException) failure;
+      apiError = new ApiError(base.getStatusCode(), base.getTimestamp(), base.getMessage());
+    } else {
+      apiError = new ApiError(500, Instant.now(), "Something went wrong. Please, try later.");
+    }
+
+    ctx.response().setStatusCode(apiError.getStatus()).end(Json.encode(apiError));
+  }
+
+  private MongoClient createMongoClient() {
+    Properties props = readProps();
+
+    assert props != null;
     return MongoClient.createShared(
       vertx,
       new JsonObject()
         .put("useObjectId", false)
-        .put("connection_string", "mongodb+srv://betacom_owner:ajYtty9KvTBX9Jml@cluster0.0rioy.mongodb.net/betacom_db?retryWrites=true&w=majority")
+        .put("connection_string", props.getProperty("connection_string"))
     );
+  }
+
+  private JWTAuth createJwtProvider() {
+    Properties props = readProps();
+    assert props != null;
+    JWTAuthOptions config = new JWTAuthOptions()
+      .setKeyStore(
+        new KeyStoreOptions()
+          .setPath("store.pkcs12")
+          .setPassword(props.getProperty("keystore_password"))
+      );
+
+    return JWTAuth.create(vertx, config);
+  }
+
+  private Properties readProps() {
+    try (InputStream input = MainVerticle.class.getClassLoader().getResourceAsStream("app.properties")) {
+      Properties props = new Properties();
+
+      if (input == null) {
+        return null;
+      }
+
+      props.load(input);
+      return props;
+
+    } catch (IOException ex) {
+      ex.printStackTrace();
+      return null;
+    }
   }
 }
