b8b411005cfdd20b205dc3d59822cb684a07f2ef
Dominik Kreński <dominik.krenski@gmail.com>
2022-02-26 21:42:49 +0100

    ApiService updated

--- src/main/java/org/dominik/server/services/definitions/ApiService.java
+++ src/main/java/org/dominik/server/services/definitions/ApiService.java
@@ -2,9 +2,14 @@ package org.dominik.server.services.definitions;
 
 import io.vertx.core.Future;
 import io.vertx.core.json.JsonObject;
+import org.dominik.server.data.Item;
 import org.dominik.server.data.User;
+
+import java.util.List;
 
 public interface ApiService {
   Future<JsonObject> findUserByLogin(String login);
   Future<String> save(User user);
+  Future<String> save(Item item, String ownerId);
+  Future<List<JsonObject>> findAllUserItems(String ownerId);
 }
--- src/main/java/org/dominik/server/services/implementations/ApiServiceImpl.java
+++ src/main/java/org/dominik/server/services/implementations/ApiServiceImpl.java
@@ -3,10 +3,14 @@ package org.dominik.server.services.implementations;
 import io.vertx.core.Future;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.mongo.MongoClient;
+import org.dominik.server.data.Item;
 import org.dominik.server.data.User;
 import org.dominik.server.services.definitions.ApiService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.List;
+import java.util.UUID;
 
 public final class ApiServiceImpl implements ApiService {
   private static final String USER_COLLECTION = "users";
@@ -18,11 +22,13 @@ public final class ApiServiceImpl implements ApiService {
     this.client = client;
   }
 
+  @Override
   public Future<JsonObject> findUserByLogin(String login) {
     JsonObject query = new JsonObject().put("login", login);
     return client.findOne(USER_COLLECTION, query, null);
   }
 
+  @Override
   public Future<String> save(User user) {
     PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
@@ -30,8 +36,26 @@ public final class ApiServiceImpl implements ApiService {
       .save(
         USER_COLLECTION,
         new JsonObject()
+          .put("_id", UUID.randomUUID().toString())
           .put("login", user.getLogin())
           .put("password", passwordEncoder.encode(user.getPassword()))
       );
+  }
+
+  @Override
+  public Future<String> save(Item item, String ownerId) {
+    return client
+      .save(
+        ITEM_COLLECTION,
+        new JsonObject()
+          .put("_id", UUID.randomUUID().toString())
+          .put("owner", ownerId)
+          .put("name", item.getName())
+      );
+  }
+
+  @Override
+  public Future<List<JsonObject>> findAllUserItems(String ownerId) {
+    return client.find(ITEM_COLLECTION, new JsonObject().put("owner", ownerId));
   }
 }
